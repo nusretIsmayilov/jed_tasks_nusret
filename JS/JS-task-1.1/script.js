4da026ccfedd30aa8768f8ae9e3f7f90f3af9786
nusretIsmayilov <nusretismayilov02@gmail.com>
2022-10-11 11:48:09 +0400

    duzbucaqli

---
+++ JS/JS-task-1.1/script.js
@@ -0,0 +1,8 @@
+function Multiply() {
+    var a = document.getElementById("txtA").value;
+    
+    var b = document.getElementById("txtB").value;
+    
+    var result = parseInt(a) * parseInt(b);
+    alert(result);
+}
