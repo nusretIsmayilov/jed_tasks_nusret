41160f4a76e2dc1070ccb11257dd5cd179c6a5cb
nusretIsmayilov <nusretismayilov02@gmail.com>
2022-11-08 22:56:32 +0400

    dom

---
+++ JS/JS-task-DOM/app.js
@@ -0,0 +1,16 @@
+const btn1 = document.getElementById('btn1')
+const bodim = document.querySelector('body')
+
+btn1.onclick = () => { 
+    bodim.style.backgroundColor='red'
+}
+btn2.onclick = () => { 
+    bodim.style.backgroundColor='blue'
+}
+btn3.onclick = () => { 
+    bodim.style.backgroundColor='pink'
+}
+btn4.onclick = () => { 
+    bodim.style.backgroundColor='yellow'
+}
+
